7d901e0ea33d718ca0ae50c65408868ca5894486
jonasnick <jonas-david.nick@student.uni-tuebingen.de>
2013-02-06 10:55:20 +0100

    Generalized the shortestPath function. Search now runs maxSteps iterations.

--- AStar.java
+++ AStar.java
@@ -4,14 +4,16 @@ import java.util.*;
 /**
  * Uses the A* Algorithm to find the shortest path from 
  * an initial to a goal node.Nodes have to implement the
- * ISearchNode interface. 
+ * ISearchNode and IGoalNode interface respectively. 
  *
  */
 public class AStar {
-    // amount of debug output
-    private static int verbose = 0;
-    public AStar() {
-    }
+    // Amount of debug output 0,1,2
+    private static int verbose = 2;
+    // The maximum number of completed nodes. After that number the algorithm returns null.
+    // If negative, the search will run until the goal node is found.
+    private static int maxSteps = 5;
+
     /**
      * @param initialNode start of the search
      * @param goalNode end of the search
@@ -21,15 +23,19 @@ public class AStar {
         PriorityQueue<ISearchNode> openSet = new PriorityQueue<ISearchNode>();
         openSet.add(initialNode);
         ArrayList<ISearchNode> closedSet = new ArrayList<ISearchNode>();
+        // current iteration of the search
+        int i = 0;
 
-        while(openSet.size() > 0) {
-            //get element with the least heuristic costs
+        while(openSet.size() > 0 && (maxSteps < 0 || i < maxSteps)) {
+            //get element with the least sum of costs from the initial node 
+            //and heuristic costs to the goal 
             ISearchNode currentNode = openSet.remove();
-            if(verbose > 0){
-                System.out.println("Open set: " + openSet.toString());
-                System.out.println("Current node: " + currentNode.toString());
-                System.out.println("Closed set: " + closedSet.toString());
-            }
+
+            //debug output according to verbose
+            System.out.println((verbose>1 ? "Open set: " + openSet.toString() + "\n" : "") 
+                        + (verbose>0 ? "Current node: "+currentNode.toString()+"\n": "")
+                        + (verbose>1 ? "Closed set: " + closedSet.toString() : ""));
+
             if(goalNode.inGoal(currentNode)) {
                 //we know the shortest path to the goal node, done
                 return currentNode;
@@ -48,8 +54,10 @@ public class AStar {
                 if(discSuccessorNode != null) {
                     successorNode = discSuccessorNode;
                     inOpenSet = true;
+                    /* System.out.println("In open set!"); */
                 } else {
                     inOpenSet = false;
+                    /* System.out.println("Not in open set"); */
                 }
                 //compute tentativeG
                 double tentativeG = currentNode.g() + currentNode.c(successorNode);
@@ -69,8 +77,30 @@ public class AStar {
                 }
             }
             closedSet.add(currentNode);
+            i += 1;
         }
         return null;
+    }
+
+    /**
+     * returns path from the earliest ancestor to the node in the argument
+     * if the parents are set via AStar search, it will return the path found.
+     * This is the shortest shortes path, if the heurstic h does not overestimate 
+     * the true remaining costs
+     * @param node node from which the parents are to be found. Parents of the node should
+     *              have been properly set in preprocessing (f.e. AStar.search)
+     * @return path to the node in the argument
+     */
+    public static ArrayList<ISearchNode> path(ISearchNode node) {
+        ArrayList<ISearchNode> path = new ArrayList<ISearchNode>();
+        path.add(node);
+        ISearchNode currentNode = node;
+        while(currentNode.getParent() != null) {
+            ISearchNode parent = currentNode.getParent();
+            path.add(0, parent);
+            currentNode = parent;
+        }
+        return path; 
     }
 
     /**
@@ -86,24 +116,5 @@ public class AStar {
             }
         }
         return null;
-    }
-
-
-    /**
-     * returns shortest path
-     * first element is the initial node and the last element is the goal node
-     * @param node goalNode a goal node after a search call
-     * @return path to the goal node
-     */
-    public static ArrayList<ISearchNode> shortestPath(ISearchNode node) {
-        ArrayList<ISearchNode> path = new ArrayList<ISearchNode>();
-        path.add(node);
-        ISearchNode currentNode = node;
-        while(currentNode.getParent() != null) {
-            ISearchNode parent = currentNode.getParent();
-            path.add(0, parent);
-            currentNode = parent;
-        }
-        return path; 
     }
 }
--- Tests/AStarTest.java
+++ Tests/AStarTest.java
@@ -12,14 +12,14 @@ public class AStarTest {
         GoalNode2D goalNode = new GoalNode2D(3, 3);
         SearchNode2D initialNode = new SearchNode2D(1, 1, null, goalNode);
         ISearchNode lastNode = AStar.search(initialNode, goalNode);
-        ArrayList<ISearchNode> path = AStar.shortestPath(lastNode);
+        ArrayList<ISearchNode> path = AStar.path(lastNode);
         assertEquals(path.size(), 5);
     }
     @Test
     public void SearchNodeCityTest() {
         ISearchNode initialNode = new SearchNodeCity("Saarbrücken");
         ISearchNode lastNode = AStar.search(initialNode, new GoalNodeCity("Würzburg"));
-        ArrayList<ISearchNode> path = AStar.shortestPath(lastNode);
+        ArrayList<ISearchNode> path = AStar.path(lastNode);
         double e = 0.00001;
         assertEquals(path.get(0).f(), 222.0, e);
         assertEquals(path.get(1).f(), 228, e);
